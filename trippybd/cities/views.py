4fd28d05c73001bc5f30cfbdb21138ca3821d4c5
saifsahed01 <saif.sahed.2906@gmail.com>
2026-04-19 00:35:34 +0600

    more or less updated

--- trippybd/cities/views.py
+++ trippybd/cities/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import City
+from hotels.models import Hotel
+
 
 # View for the city list page
 def city_list(request):
@@ -8,5 +10,10 @@ def city_list(request):
 
 # View for the city details page
 def city_details(request, city_id):
-    city = get_object_or_404(City, id=city_id)  # Fetch the city by id
-    return render(request, 'cities/city_details.html', {'city': city})
+    # Fetch the city using the city_id, or show a 404 page if it doesn't exist
+    city = get_object_or_404(City, id=city_id)
+    
+    # Fetch all hotels related to this city
+    hotels = Hotel.objects.filter(city=city)
+    
+    return render(request, 'cities/city_details.html', {'city': city, 'hotels': hotels})
